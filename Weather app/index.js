373dcd1ef199f088c30265f8fcc471e6dcd6b3bf
farhaan <farhaanfashia28@gmail.com>
2024-03-13 21:42:19 +0530

    API is working!!

--- Weather app/index.js
+++ Weather app/index.js
@@ -10,10 +10,12 @@ let wind_speed = document.querySelector(".wind-speed .values");
 let inputbox = document.querySelector(".input-feild");
 let searchbtn = document.querySelector("#search-icon");
 
-function getWeather(city){
+async function getWeather(city){
     const api_key = "adfb7cb06794efbc748243714c739abe";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
+    const weather_data = await fetch(url).then(value => value.json());
+    // console.log(weather_data);
 }
 
 searchbtn.addEventListener("click",function(){
