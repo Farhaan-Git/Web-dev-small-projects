b63b05bc1d697bab06e9649a013055cd6e4d02b1
farhaan <farhaanfashia28@gmail.com>
2024-03-13 17:42:06 +0530

    Creation of the HTML and CSS for weather app

---
+++ Weather app/index.js
@@ -0,0 +1,21 @@
+let card_city_name = document.querySelector("#card-city-name");
+let weather_img = document.querySelector("#weather-img");
+let degree = document.querySelector("#degree");
+let weather_in_text = document.querySelector("#weather-in-text");
+let humidity = document.querySelector(".humidity .values");
+let wind_speed = document.querySelector(".wind-speed .values");
+
+// input values
+
+let inputbox = document.querySelector(".input-feild");
+let searchbtn = document.querySelector("#search-icon");
+
+function getWeather(city){
+    const api_key = "adfb7cb06794efbc748243714c739abe";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+}
+
+searchbtn.addEventListener("click",function(){
+    getWeather(inputbox.value);
+});
