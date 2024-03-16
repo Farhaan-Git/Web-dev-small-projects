00d05e11fa088977eeab09f811ddd1ea48e57541
farhaan <farhaanfashia28@gmail.com>
2024-03-17 01:13:11 +0530

    weather app V1.0

--- Weather app/index.js
+++ Weather app/index.js
@@ -10,14 +10,32 @@ let wind_speed = document.querySelector(".wind-speed .values");
 let inputbox = document.querySelector(".input-feild");
 let searchbtn = document.querySelector("#search-icon");
 
+
 async function getWeather(city){
     const api_key = "adfb7cb06794efbc748243714c739abe";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
     const weather_data = await fetch(url).then(value => value.json());
+    if(weather_data.message==="city not found"){
+        alert("Wrong input city name!");
+        return;
+    }
     // console.log(weather_data);
+    card_city_name.innerHTML= weather_data.name;
+    degree.innerHTML = `${Math.ceil(weather_data.main.temp-275.15)} °C`;
+    wind_speed.innerHTML = `${weather_data.wind.speed} Km/H`;
+    weather_in_text.innerHTML = `${weather_data.weather[0].description}`;
+    humidity.innerHTML = `${weather_data.main.humidity}%`;
+    weather_img.setAttribute("src",`images/${weather_data.weather[0].description}.png`);
+
+
 }
 
 searchbtn.addEventListener("click",function(){
     getWeather(inputbox.value);
+});
+document.addEventListener("keydown",(event)=>{
+    if(event.key === "Enter"){
+        getWeather(inputbox.value);
+    }
 });
