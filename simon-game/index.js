dbabb956fd8ee30d2d4f788c4701b20122405987
farhaan <farhaanfashia28@gmail.com>
2024-03-25 22:52:58 +0530

    initial js code added

--- simon-game/index.js
+++ simon-game/index.js
@@ -1,32 +1,122 @@
 let title = $("#title");
 let arr = ["green","red","yellow","blue","green","red","yellow","blue","green","red","yellow","blue"];
 let ans = [];
+let userans = [];
+let ptr_userans = 0;
+let ptr_ans = 0;
 let level = 1;
-function start_game(){
-    $(document).keydown((event)=>{ 
-        if(event.key===" "){
-            $("#start").css("visibility","hidden");
-            $("#container").removeClass("startclass");
-        }
-    });
-    return true;
+
+let spacebarEventListener = $(document).on("keydown", (event) => {
+    if (event.key === " ") {
+        $("#start").css("visibility", "hidden");
+        $("#container").removeClass("startclass");
+        $(document).off("keydown", spacebarEventListener); 
+        showAns();
+        start();
+    }
+});
+
+
+
+async function start(){
+    let validation = null;
+    do{
+        validation = await getUser();
+        ptr_ans++;
+        ptr_userans++;
+        showAns();
+    }while(validation);
 }
 
-// $(".box").click(validation());
+
+
 function  getRandom_Value(){
     return arr[Math.floor(Math.random()*arr.length)];
 }
+async function getUser(){
+    let val = null;
+    val = $("#green").on("click",()=>{
+        // $("#green").off("click",val);
+        userans.push("green");
+        return new Promise((resolve,reject)=>{
+        if(validate(ptr_ans,ptr_userans)){
+            resolve(true);
+        }
+        else{
+            reject(false);
+        }
+        });
+    });
+    val = $("#red").on("click",()=>{
+        // $("#green").off("click",val);
+        userans.push("red");
+        return new Promise((resolve,reject)=>{
+        if(validate(ptr_ans,ptr_userans)){
+            resolve(true);
+        }
+        else{
+            reject(false);
+        }
+        });
+    });
+    val = $("#yellow").on("click",()=>{
+        // $("#green").off("click",val);
+        userans.push("yellow");
+        return new Promise((resolve,reject)=>{
+        if(validate(ptr_ans,ptr_userans)){
+            resolve(true);
+        }
+        else{
+            reject(false);
+        }
+        });
+    });
+    val = $("#blue").on("click",()=>{
+        // $("#green").off("click",val);
+        userans.push("blue");
+        return new Promise((resolve,reject)=>{
+        if(validate(ptr_ans,ptr_userans)){
+            resolve(true);
+        }
+        else{
+            reject(false);
+        }
+        });
+    });  
+
+    
+}
+
+async function validate(ptr1,ptr2){
+        if(ans[ptr2]==userans[ptr1]){
+        //    let srt = document.querySelector("#title").innerHTML.slice(srt.length-1) = ptr2+1; 
+            console.log("correct answer!");
+            level++;
+            return true;
+            // ptr2++;
+            // await showAns();
+            // return ++ptr2;
+        }
+        else{
+            console.log("gameover");
+            return false;
+            // return ++ptr2;
+        }
+}
 
 function showAns() {
-    let index = 0;
-    let interval = setInterval(() => {
-        if (index >= ans.length) {
-            clearInterval(interval);
-            return;
-        }
-        blink(ans[index]);
-        index++;
-    }, 1000); // Adjust the delay between each blink here (in milliseconds)
+    return new Promise((resolve,reject)=>{
+        ans.push(getRandom_Value());
+        let index = 0;
+        let interval = setInterval(() => {
+            if (index >= ans.length) {
+                clearInterval(interval);
+                return;
+            }
+            blink(ans[index]);
+            index++;
+        }, 1000);
+    });
 }
 
  function blink(val){
@@ -35,14 +125,7 @@ function showAns() {
         $("#"+val).removeClass("select");
     }, 500); 
 }
+    
 
+  
 
-if(start_game()){
-    let i = 0;
-    while(i!=3){
-        ans.push(getRandom_Value());
-        i++;
-    }
-    console.log(ans);
-    showAns();
-}
